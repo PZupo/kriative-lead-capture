de48b84e06231f06708708649b45a569900382a3
PZupo <114028453+PZupo@users.noreply.github.com>
2025-10-27 15:30:37 -0300

    Create Navbar.tsx

---
+++ src/ui/Navbar.tsx
@@ -0,0 +1,9 @@
+export default function Navbar() {
+  return (
+    <header className="w-full py-4 bg-gradient-to-r from-teal to-orange shadow-sm">
+      <h1 className="text-center font-bold text-xl text-white">
+        Kriative Lead Capture
+      </h1>
+    </header>
+  )
+}
